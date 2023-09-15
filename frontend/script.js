806ee6217dd9bdce03cb017cd5649d7bc107d95f
evelynvaida <113584134+evelynvaida@users.noreply.github.com>
2023-09-15 21:45:01 +0300

    basic layout and JS practice

---
+++ frontend/script.js
@@ -0,0 +1,12 @@
+console.log("Hello World");
+console.log("I am Evelyn");
+//setInterval(()=>console.log("Hello"),1000);
+
+let x = 10;
+let y = 5;
+//console.log(x+y);
+x+y;
+let z = "csesze";
+console.log(z);
+z = "bogre";
+console.log(z);
